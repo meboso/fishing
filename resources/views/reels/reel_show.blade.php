0fdcaefdbcb1e595143c2193a37c6dde31848e10
meoboso <kodai.meoboso@gmail.com>
2022-03-18 12:52:54 +0900

    リールのview実装

--- resources/views/reels/reel_show.blade.php
+++ resources/views/reels/reel_show.blade.php
@@ -2,13 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>ロッド詳細</title>
+        <title>Blog</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1 class="rod_name">{{ $rod->rod_name }}</h1>
-        
+        <h1 class="reel_name">{{ $reel->reel_name }}</h1>
          <div class="footer">
             <a href="/">一覧へ戻る</a>
         </div>
